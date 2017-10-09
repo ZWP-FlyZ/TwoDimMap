d259d44ccdaedfb383c1bcc1684c3183b024d25b
zwp <1258271806@qq.com>
2017-10-09 11:14:16 +0800

    添加文档，初步完成1.0版

--- src/main/java/com/zwp/flyz/interfaces/TwoDimMap.java
+++ src/main/java/com/zwp/flyz/interfaces/TwoDimMap.java
@@ -13,24 +13,75 @@ import java.util.Set;
 
 public interface TwoDimMap<X,Y,V> {
 	
+	/**
+	 * 
+	 * @param x the key x
+	 * @param y the key y
+	 * @return the value of key (x,y).
+	 */
 	public V get(Object x,Object y);
 	
+	/**
+	 * 
+	 * @param x the key x
+	 * @param y the key y
+	 * @param defV the default value if not contain value
+	 * @return
+	 */
 	public V get(Object x,Object y, V defV);
 	
+	/**
+	 * 
+	 * @param x
+	 * @param y
+	 * @param v
+	 * @return
+	 */
 	public V put(X x,Y y,V v);
 	
+	/**
+	 * 
+	 * @param x
+	 * @param y
+	 * @return
+	 */
 	public V remove(Object x,Object y);
 	
+	/**
+	 * 
+	 * @param x
+	 * @param y
+	 * @return
+	 */
 	public boolean isContain(Object x,Object y);
 	
+	/**
+	 * clear all elements
+	 */
 	public void clear();
 	
+	/**
+	 * 
+	 * @return the number of all elements
+	 */
 	public int size();
 	
+	/**
+	 * 
+	 * @return return true is the map is empty
+	 */
 	public boolean isEmpty();
 	
+	/**
+	 * 
+	 * @return all values
+	 */
 	public Collection<V> values();
 	
+	/**
+	 * 
+	 * @return all keys (x,y)
+	 */
 	public Set<TwoDimKey<X,Y>> keySet();
 	
 	public Set<TwoDimEntry<X,Y,V>> entrySet();
